0337c0f34eadbfbbfbfd98cf7937f142725dc77b
rivercity310 <h970126@gmail.com>
2024-07-22 04:18:33 +0900

    feat: 개발 환경 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 function App() {
-
-  return (
-    <div className="font-bold text-[36px] bg-gray-700">Hi</div>
-  );
+  return <div className="font-bold text-[36px] bg-gray-700">Hi</div>;
 }
 
 export default App;
